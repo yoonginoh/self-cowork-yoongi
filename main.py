d014ef407e2f6c3f991fdeeccdcd179d99cae961
DESKTOP-AEBPAVF\202-1576 <ksjnote@naver.com>
2023-07-06 10:33:04 +0900

    한번 더하는 기능 추가

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import random
 
-print("로또번호다섯개추첨합시다!^^")
+print("로또번호다섯개추첨해!!^^")
 
 for i in range(5) :
     lotto = random.randint((1,46),6)
     print(lotto)    
+
+y = input("다시 추첨하기 원하면 y를 누르세요 :")
+
+if y == "y" :
+    lotto = random.sample(range(1,46), 6)
+    print(lotto)
+
